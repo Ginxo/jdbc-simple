8804b9f8ac1f1d5311273589d4d29c19c3c4cb32
Enrique Mingorance Cano <ginxo@hotmail.com>
2018-07-03 17:12:11 +0200

    ConnectionManager

---
+++ src/main/java/com/everis/alicante/eoi/jdbc/ConnectionManager.java
@@ -0,0 +1,35 @@
+package com.everis.alicante.eoi.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+
+    private static ConnectionManager connectionManager;
+
+    private Connection connection;
+
+    private ConnectionManager() {
+        try {
+            this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/exercise1", "root", "password");
+        } catch (SQLException e) {
+            throw new RuntimeException("can´t connect", e);
+        }
+    }
+
+    public static ConnectionManager getInstance() {
+        if (connectionManager == null) {
+            connectionManager = new ConnectionManager();
+        }
+        return connectionManager;
+    }
+
+    public void close() {
+        try {
+            this.connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException("can´t close", e);
+        }
+    }
+}
